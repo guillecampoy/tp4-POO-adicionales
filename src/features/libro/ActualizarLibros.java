590cbe6b3e6057cbfd75eca531006fabc7545aaa
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 13:18:50 -0300

    Refactor para re utilizar creación de libros base para purebas

--- src/features/libro/ActualizarLibros.java
+++ src/features/libro/ActualizarLibros.java
@@ -8,34 +8,54 @@ import utils.UtilsColor;
 public class ActualizarLibros implements Ejecutable {
     @Override
     public void execute() {
+        CrearLibros librosBase = new CrearLibros();
+        // Libros base utilizados
+        Libro primerLibro = librosBase.getLibroIndependiente();
+        Libro segundoLibro = librosBase.getSegundoLibroIndependiente();
+        Libro tercerLibro = librosBase.getCompleto();
+        Libro cuartoLibro = librosBase.getOtroCompleto();
+
         // Creación de libros de prueba
-        String tituloLibro = "Cuentos de amor, de locura y de muerte";
-        String autorLibro = "Horacio Quiroga";
-        String nuevoTituloPrefijo = "Edición ilustrada 2025";
-        String tituloLibroB = "El principito";
-        String autorLibroB = "Saint-Exupéry";
-        String nuevoTituloB = "Le Petit Prince (El Principito)";
-        Libro libroPrueba = new Libro(tituloLibro, autorLibro);
-        Libro libroPruebaB = new Libro(tituloLibroB, autorLibroB);
+        String libroUnoPrefijo = "Edición ilustrada 2025";
+        String libroDosNuevoTitulo = "Cuentos de amor, de locura, de muerte, edición infantil";
+        String libroTresPrefijo = "Edición 50ª aniversario";
+        String libroCuatroNuevoTitulo ="Le Petit Prince (El Principito)";
+
 
         UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Libros de prueba creados..");
-        UtilsColor.imprimirBloque(ContextColor.DEFAULT, libroPrueba.toString());
-        UtilsColor.imprimirBloque(ContextColor.DEFAULT, libroPruebaB.toString());
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, primerLibro.toString());
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, segundoLibro.toString());
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, tercerLibro.toString());
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, cuartoLibro.toString());
 
         UtilsColor.imprimirEncabezado();
         UtilsColor.imprimirBloque(ContextColor.INFO, "Actualizaciones de títulos");
         //Primer Libro
-        UtilsColor.imprimirBloque(ContextColor.INFO, "\nPrimer Libro, estrrategia prefijo");
-        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+libroPrueba.getTitulo());
+        UtilsColor.imprimirBloque(ContextColor.INFO, "\nPrimer Libro, estrategia prefijo");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+primerLibro.getTitulo());
         //Actualizo
-        libroPrueba.actualizarConPrefijo(nuevoTituloPrefijo);
-        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+libroPrueba.getTitulo());
+        primerLibro.actualizarConPrefijo(libroUnoPrefijo);
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+primerLibro.getTitulo());
 
-        //SegundoLibro
-        UtilsColor.imprimirBloque(ContextColor.INFO, "\nSegundo Libro, estrrategia nuevo título");
-        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+libroPruebaB.getTitulo());
+        //Segundo Libro
+        UtilsColor.imprimirBloque(ContextColor.INFO, "\nSegundo Libro, estrategia nuevo título");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+segundoLibro.getTitulo());
         //Actualizo
-        libroPruebaB.actualizarTitulo(nuevoTituloB);
-        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+libroPruebaB.getTitulo());
+        segundoLibro.actualizarTitulo(libroDosNuevoTitulo);
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+segundoLibro.getTitulo());
+
+        //Tercer Libro
+        UtilsColor.imprimirBloque(ContextColor.INFO, "\nTercer Libro, estrategia prefijo");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+tercerLibro.getTitulo());
+        //Actualizo
+        tercerLibro.actualizarConPrefijo(libroTresPrefijo);
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+tercerLibro.getTitulo());
+
+        //Cuarto Libro
+        UtilsColor.imprimirBloque(ContextColor.INFO, "\nCuarto Libro, estrategia nuevo título");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Nombre actual libro: "+cuartoLibro.getTitulo());
+        //Actualizo
+        cuartoLibro.actualizarTitulo(libroCuatroNuevoTitulo);
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Nuevo título: "+cuartoLibro.getTitulo());
     }
 }
