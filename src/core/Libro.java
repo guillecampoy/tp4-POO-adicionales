e626e18276e5d23eaea55c31620304c8e12de247
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 11:30:46 -0300

    Implements first use case Use diferents strategies to create new "Libros"

--- src/core/Libro.java
+++ src/core/Libro.java
@@ -1,4 +1,59 @@
 package core;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private static String editorial;
+
+    // Constantes
+    static final String EDITORIOAL_POR_DEFECTO= "Independiente";
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        cambiarEditorial(EDITORIOAL_POR_DEFECTO);
+    }
+
+    public Libro(String titulo, String autor, String editorial) {
+        this.titulo = titulo;
+        this.autor = autor;
+        cambiarEditorial(editorial);
+    }
+
+    // Getters and Setters
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getEditorial() {
+        return editorial;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editorial='" + editorial + '\'' +
+                '}';
+    }
+
+    public void actualizarTitulo(String nuevoTitulo) {
+        this.titulo = nuevoTitulo;
+    }
+
+    public void actualizarConPrefijo(String prefijo) {
+        this.titulo = prefijo+this.titulo;
+    }
+    public static void cambiarEditorial(String nueva) {
+        Libro.editorial = nueva;
+    }
 }
--- src/features/libro/CrearLibros.java
+++ src/features/libro/CrearLibros.java
@@ -1,10 +1,40 @@
 package features.libro;
 
 import core.Ejecutable;
+import core.Libro;
+import utils.ContextColor;
+import utils.UtilsColor;
+
+import java.awt.*;
 
 public class CrearLibros implements Ejecutable {
     @Override
     public void execute() {
+        // Definición variables base construcción libros
+        String nombreLibroIndependiente = "Desierto de piedra";
+        String nombreLibroIndependienteB = "Cuentos de amor, de locura y de muerte";
+        String nombreTituloCompleto = "Martín Fierro";
+        String nombreOtroLibroCompleto = "El principito";
+        String autorLibroIndependiente = "Hugo Wast";
+        String autorLibroIndependienteB = "Horacio Quiroga";
+        String autorTituloCompleto = "José Hernández";
+        String autorOtroLibroCompleto = "Saint-Exupéry";
+        String editorialLibroCompleto = "Gauchesca";
+        String editorialOtroLibroCompleto = "Viva la France!";
+
+        // Uso de ambas formas de constructores
+        Libro libroIndependiente = new Libro(nombreLibroIndependiente, autorLibroIndependiente);
+        Libro segundoLibroIndependiente = new Libro(nombreLibroIndependienteB, autorLibroIndependienteB);
+        Libro completo = new Libro(nombreTituloCompleto, autorTituloCompleto, editorialLibroCompleto);
+        Libro otroCompleto = new Libro(nombreOtroLibroCompleto, autorOtroLibroCompleto, editorialOtroLibroCompleto);
+
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Libros creados...");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "\nLibro editorial independiente"+libroIndependiente);
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Segundo libro editorial independiente"+segundoLibroIndependiente);
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Libro + editorial"+completo);
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Segundo libro + editorial"+otroCompleto);
+
+
 
     }
 }
