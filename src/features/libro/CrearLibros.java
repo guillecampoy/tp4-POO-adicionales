b8fb3807a054c17e9025bd6b3e6c234c71b4c4d3
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 11:51:46 -0300

    Se implementa tercer Caso de Uso Representa la actualización de atributo estático que representa la editorial Se re utiliza clase de creación base de libros para favorecer la re utilización de código realizando un pequeño refactor y creando los métodos pertinentes para poder consumir los libros creados desde otra clase

--- src/features/libro/CrearLibros.java
+++ src/features/libro/CrearLibros.java
@@ -6,30 +6,43 @@ import utils.ContextColor;
 import utils.UtilsColor;
 
 public class CrearLibros implements Ejecutable {
+    // Definición variables base construcción libros
+    String nombreLibroIndependiente = "Desierto de piedra";
+    String nombreLibroIndependienteB = "Cuentos de amor, de locura y de muerte";
+    String nombreTituloCompleto = "Martín Fierro";
+    String nombreOtroLibroCompleto = "El principito";
+    String autorLibroIndependiente = "Hugo Wast";
+    String autorLibroIndependienteB = "Horacio Quiroga";
+    String autorTituloCompleto = "José Hernández";
+    String autorOtroLibroCompleto = "Saint-Exupéry";
+    String editorialLibroCompleto = "Gauchesca";
+    String editorialOtroLibroCompleto = "Viva la France!";
+
+    // Uso de ambas formas de constructores
+    private final Libro libroIndependiente = new Libro(nombreLibroIndependiente, autorLibroIndependiente);
+    private final Libro segundoLibroIndependiente = new Libro(nombreLibroIndependienteB, autorLibroIndependienteB);
+    private final Libro completo = new Libro(nombreTituloCompleto, autorTituloCompleto, editorialLibroCompleto);
+    private final Libro otroCompleto = new Libro(nombreOtroLibroCompleto, autorOtroLibroCompleto, editorialOtroLibroCompleto);
+
     @Override
     public void execute() {
-        // Definición variables base construcción libros
-        String nombreLibroIndependiente = "Desierto de piedra";
-        String nombreLibroIndependienteB = "Cuentos de amor, de locura y de muerte";
-        String nombreTituloCompleto = "Martín Fierro";
-        String nombreOtroLibroCompleto = "El principito";
-        String autorLibroIndependiente = "Hugo Wast";
-        String autorLibroIndependienteB = "Horacio Quiroga";
-        String autorTituloCompleto = "José Hernández";
-        String autorOtroLibroCompleto = "Saint-Exupéry";
-        String editorialLibroCompleto = "Gauchesca";
-        String editorialOtroLibroCompleto = "Viva la France!";
-
-        // Uso de ambas formas de constructores
-        Libro libroIndependiente = new Libro(nombreLibroIndependiente, autorLibroIndependiente);
-        Libro segundoLibroIndependiente = new Libro(nombreLibroIndependienteB, autorLibroIndependienteB);
-        Libro completo = new Libro(nombreTituloCompleto, autorTituloCompleto, editorialLibroCompleto);
-        Libro otroCompleto = new Libro(nombreOtroLibroCompleto, autorOtroLibroCompleto, editorialOtroLibroCompleto);
-
         UtilsColor.imprimirBloque(ContextColor.INFO, "Libros creados...");
         UtilsColor.imprimirBloque(ContextColor.DEFAULT, "\nLibro título + autor "+libroIndependiente);
         UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Segundo título + autor "+segundoLibroIndependiente);
         UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Titulo + autor + editorial "+completo);
         UtilsColor.imprimirBloque(ContextColor.DEFAULT, "Segundo título + autor + editorial "+otroCompleto);
+    }
+
+    public  Libro getLibroIndependiente() {
+        return libroIndependiente;
+    }
+    public  Libro getSegundoLibroIndependiente() {
+        return segundoLibroIndependiente;
+    }
+    public Libro getCompleto() {
+        return completo;
+    }
+    public Libro getOtroCompleto() {
+        return otroCompleto;
     }
 }
