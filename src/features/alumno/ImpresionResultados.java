88c876d1e3f8068aeb64ad8ab2cd40f37510a037
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 17:33:44 -0300

    Impremento tercer caso de uso Alumnno Se implementa un listado generando un set de datos de prueba Se itera lis lita con lógica para determinar quienes aprobaron y quienes no

--- src/features/alumno/ImpresionResultados.java
+++ src/features/alumno/ImpresionResultados.java
@@ -1,10 +1,31 @@
 package features.alumno;
 
+import core.Alumno;
 import core.Ejecutable;
+import utils.ContextColor;
+import utils.UtilsColor;
 
 public class ImpresionResultados implements Ejecutable {
+    // Crear alumnos
+    CrearAlumnos alumnos = new CrearAlumnos();
+
+    // Alumnos
+    Alumno primerAlumno = alumnos.getAlumnoUno();
+    Alumno segundoAlumno = alumnos.getAlumnoDos();
+    Alumno tercerAlumno = alumnos.getAlumnoTres();
+    Alumno cuartoAlumno = alumnos.getAlumnoCuatro();
+
+    //Almaceno en un listado para ser más legible
+    Alumno[] listadoAlumnos = { primerAlumno, segundoAlumno, tercerAlumno, cuartoAlumno };
+
     @Override
     public void execute() {
-
+        for (Alumno alumno : listadoAlumnos) {
+            if (alumno.aprobo()){
+                UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Alumno APROBADO: "+alumno);
+            } else {
+                UtilsColor.imprimirBloque(ContextColor.ERROR, "Alumno DESAPROBADO: "+alumno);
+            }
+        }
     }
 }
