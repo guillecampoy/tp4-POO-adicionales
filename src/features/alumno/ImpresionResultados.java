0e4e4878eda01eb7766ee201edbe77cbc2832d22
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 17:53:55 -0300

    refactor para utilizar lógica de listado

--- src/features/alumno/ImpresionResultados.java
+++ src/features/alumno/ImpresionResultados.java
@@ -5,27 +5,36 @@ import core.Ejecutable;
 import utils.ContextColor;
 import utils.UtilsColor;
 
+import java.util.List;
+
 public class ImpresionResultados implements Ejecutable {
-    // Crear alumnos
-    CrearAlumnos alumnos = new CrearAlumnos();
+    public List<Alumno> crearListado () {
+        // Crear alumnos
+        CrearAlumnos alumnos = new CrearAlumnos();
 
-    // Alumnos
-    Alumno primerAlumno = alumnos.getAlumnoUno();
-    Alumno segundoAlumno = alumnos.getAlumnoDos();
-    Alumno tercerAlumno = alumnos.getAlumnoTres();
-    Alumno cuartoAlumno = alumnos.getAlumnoCuatro();
+        // Alumnos
+        Alumno primerAlumno = alumnos.getAlumnoUno();
+        Alumno segundoAlumno = alumnos.getAlumnoDos();
+        Alumno tercerAlumno = alumnos.getAlumnoTres();
+        Alumno cuartoAlumno = alumnos.getAlumnoCuatro();
 
-    //Almaceno en un listado para ser más legible
-    Alumno[] listadoAlumnos = { primerAlumno, segundoAlumno, tercerAlumno, cuartoAlumno };
+        return List.of(primerAlumno, segundoAlumno, tercerAlumno, cuartoAlumno);
+    }
+
+
+    public void listarDiscriminadoAprobados (List<Alumno> listadoAlumnos) {
+        listadoAlumnos.forEach(alumno -> {
+            if (alumno.aprobo()) {
+                UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Alumno APROBADO: " + alumno);
+            } else {
+                UtilsColor.imprimirBloque(ContextColor.ERROR, "Alumno DESAPROBADO: " + alumno);
+            }
+        });
+    }
 
     @Override
     public void execute() {
-        for (Alumno alumno : listadoAlumnos) {
-            if (alumno.aprobo()){
-                UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Alumno APROBADO: "+alumno);
-            } else {
-                UtilsColor.imprimirBloque(ContextColor.ERROR, "Alumno DESAPROBADO: "+alumno);
-            }
-        }
+        List<Alumno> listadoAlumnos = crearListado();
+        listarDiscriminadoAprobados(listadoAlumnos);
     }
 }
