58c722306de328b94e17afdf8670ff229b267126
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-22 12:46:10 -0300

    Implemento clase Alumno con estructura propuesta en la entrega Como complemento se ajusta el formato de presentación de resultados para formatear el promedio

---
+++ src/core/Alumno.java
@@ -0,0 +1,57 @@
+package core;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Alumno {
+    private String nombre;
+    private double promedio;
+
+    //Constantes
+    private static double NOTA_APROBACION = 6.00;
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+        this.promedio = 0;
+    }
+    public Alumno(String nombre, double promedio) {
+        this.nombre = nombre;
+        this.promedio = promedio;
+    }
+
+    public void actualizarPromedio (double promedio) {
+        this.promedio = promedio;
+    }
+
+    public void actualizarPromedio (double [] notas) {
+        if (notas == null || notas.length == 0 ) {
+            // no hay nada que hacer en este caso
+            return;
+        }
+        double suma = 0;
+        for (double nota : notas) {
+            suma +=nota;
+        }
+        this.promedio = suma / notas.length;
+    }
+
+    public boolean aprobo() {
+        return promedio >= NOTA_APROBACION;
+    }
+
+    public void cambioNotaAprobacion(double nuevaNotaAprobacion) {
+        NOTA_APROBACION = nuevaNotaAprobacion;
+    }
+
+    @Override
+    public String toString() {
+        Locale ar = Locale.of("es", "AR");
+        NumberFormat nf = NumberFormat.getCurrencyInstance(ar);
+        nf.setMinimumFractionDigits(2);
+        nf.setMaximumFractionDigits(2);
+        nf.setGroupingUsed(true);
+        return "Alumno{nombre='" + nombre + "', promedio=" + nf.format(promedio) + "aprobó: "+aprobo()+'}';
+    }
+
+}
